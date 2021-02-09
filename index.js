a32f881d632d812def3503e1fdd9c8d2d5072faf
Blaine <b.atsbeha@gmail.com>
2021-02-09 08:39:09 -0800

    env and gitignore added, connection between backend and db lost

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3001
 const db = require('./queries')
+const port = 3001
 
 app.use(bodyParser.json())
 app.use(
