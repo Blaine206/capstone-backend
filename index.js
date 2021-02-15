855f691ff9fe0232e640c907b17e29bc5005667d
Blaine <b.atsbeha@gmail.com>
2021-02-14 18:37:29 -0800

    possibly temp - deleted eventQueries.js, placed contents in queries.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
+// const db2 = require('./eventQueries')
 const port = 3001
 
 app.use(cors())
@@ -19,12 +20,18 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+// app.get('/users', db.getUsers)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
 
-  app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+app.get('/events', db.getEvents)
+app.get('/events/:id', db.getEventById)
+app.post('/events', db.createEvent)
+app.put('/events/:id', db.updateEvent)
+app.delete('/events/:id', db.deleteEvent)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
--- queries.js
+++ queries.js
@@ -14,10 +14,12 @@ const pool = new Pool({
     password: password,
     port: port,
 })
+
 console.log(Pool)
 console.log(pool)
-const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+
+const getEvents = (request, response) => {
+    pool.query('SELECT * FROM events ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
         }
@@ -25,10 +27,10 @@ const getUsers = (request, response) => {
     })
 }
 
-const getUserById = (request, response) => {
+const getEventById = (request, response) => {
     const id = parseInt(request.params.id)
 
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    pool.query('SELECT * FROM events WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error
         }
@@ -36,48 +38,132 @@ const getUserById = (request, response) => {
         })
     }
 
-const createUser = (request, response) => {
-    const { name, email } = request.body
+const createEvent = (request, response) => {
+    const { title, creator, date, location } = request.body
 
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    pool.query('INSERT INTO events (title, creator, date, location) VALUES ($1, $2, $3, $4)', [title, creator, date, location], (error, results) => {
         if (error) {
         throw error
         }
-        response.status(201).send(`User added with ID: ${results.id}`)
+        response.status(201).send(`Event added with ID: ${results.id}`)
     })
 }
 
-const updateUser = (request, response) => {
+const updateEvent = (request, response) => {
     const id = parseInt(request.params.id)
-    const { name, email } = request.body
+    const { title, creator, date, location } = request.body
 
     pool.query(
-        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-        [name, email, id],
+        'UPDATE events SET title = $1, creator = $2 , date = $3, location = $4, WHERE id = $5',
+        [title, creator, date, location, id],
         (error, results) => {
             if (error) {
                 throw error
             }
-            response.status(200).send(`User modified with ID: ${id}`)
+            response.status(200).send(`Event modified with ID: ${id}`)
         }
     )
 }
 
-const deleteUser = (request, response) => {
+const deleteEvent = (request, response) => {
     const id = parseInt(request.params.id)
 
-    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    pool.query('DELETE FROM events WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error
         }
-        response.status(200).send(`User deleted with ID: ${id}`)
+        response.status(200).send(`Event deleted with ID: ${id}`)
     })
 }
 
 module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
+    getEvents,
+    getEventById,
+    createEvent,
+    updateEvent,
+    deleteEvent,
 }
+
+// require('dotenv').config()
+// const port = process.env.API_PORT;
+// const database = process.env.API_DATABASE;
+// const host = process.env.API_HOST;
+// const user = process.env.API_USER;
+// const password = process.env.API_PASSWORD;
+// console.log(`Your port is ${port}`);
+
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: user,
+//     host: host,
+//     database: database,
+//     password: password,
+//     port: port,
+// })
+// console.log(Pool)
+// console.log(pool)
+// const getUsers = (request, response) => {
+//     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).json(results.rows)
+//     })
+// }
+
+// const getUserById = (request, response) => {
+//     const id = parseInt(request.params.id)
+
+//     pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).json(results.rows)
+//         })
+//     }
+
+// const createUser = (request, response) => {
+//     const { name, email } = request.body
+
+//     pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+//         if (error) {
+//         throw error
+//         }
+//         response.status(201).send(`User added with ID: ${results.id}`)
+//     })
+// }
+
+// const updateUser = (request, response) => {
+//     const id = parseInt(request.params.id)
+//     const { name, email } = request.body
+
+//     pool.query(
+//         'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+//         [name, email, id],
+//         (error, results) => {
+//             if (error) {
+//                 throw error
+//             }
+//             response.status(200).send(`User modified with ID: ${id}`)
+//         }
+//     )
+// }
+
+// const deleteUser = (request, response) => {
+//     const id = parseInt(request.params.id)
+
+//     pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).send(`User deleted with ID: ${id}`)
+//     })
+// }
+
+// module.exports = {
+//     getUsers,
+//     getUserById,
+//     createUser,
+//     updateUser,
+//     deleteUser,
+// }
